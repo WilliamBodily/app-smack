7d560fafa45ea023ef9afe315e5bc351d7bf131f
William Bodily <william.bodily@gmail.com>
2018-11-21 18:52:38 -0500

    Added chat and channel view controllers, setup the menu button, added a label and made the button control reveal the channel view. Swiping to open and tapping to close the channel view was included as well.

--- app-smack/smack/Controller/ChannelViewController.swift
+++ app-smack/smack/Controller/ChannelViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ChannelViewController.swift
 //  Smack
 //
 //  Created by William Bodily on 11/21/18.
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ChannelViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.revealViewController()?.rearViewRevealWidth = self.view.frame.size.width - 60
     }
 
-
 }
-
---
+++ app-smack/smack/Controller/ChatViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ChatViewController.swift
+//  Smack
+//
+//  Created by William Bodily on 11/21/18.
+//  Copyright © 2018 William Bodily. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    @IBOutlet weak var menuButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuButton.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer((self.revealViewController()?.panGestureRecognizer())!)
+        self.view.addGestureRecognizer((self.revealViewController()?.tapGestureRecognizer())!)
+    }
+
+}
