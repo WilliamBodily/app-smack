2eba396083377c5c669c306ea0ac397610f19148
William Bodily <william.bodily@gmail.com>
2018-11-21 16:06:44 -0500

    Initial project setup and check in

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by William Bodily on 11/21/18.
+//  Copyright © 2018 William Bodily. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
