7cb1f5672016006e6566d148b807bdaee6566ad1
William Bodily <william.bodily@gmail.com>
2018-11-23 19:23:48 -0500

    Created user login view controller

---
+++ app-smack/smack/Controller/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  Smack
+//
+//  Created by William Bodily on 11/23/18.
+//  Copyright © 2018 William Bodily. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ app-smack/smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by William Bodily on 11/23/18.
+//  Copyright © 2018 William Bodily. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
